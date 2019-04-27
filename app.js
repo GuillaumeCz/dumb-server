a63d33c0f0ed3d92a5d0b8c03939ffde12c3feaf
GuillaumeCz <guillaume.couzy@protonmail.com>
2019-04-27 22:58:49 +0200

    Added mongoose

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import path from 'path';
+import dotenv from 'dotenv';
 
 import indexRouter from './routes/index.route';
 
 const app = express();
 
 const PORT = 3000;
+const ENV = dotenv.config().parsed;
+
+const mongoUri = ENV.MONGO_HOST + ENV.MONGO_NAME;
+const mongoPort = ENV.MONGO_PORT;
+
+mongoose.set('useCreateIndex', true);
+mongoose.connect(mongoUri, { useNewUrlParser: true, useFindAndModify: false });
+
+const db = mongoose.connection;
+db.on('error', () => {
+  throw new Error(`Unable to connect to database: ${mongoUri}`);
+});
+
+db.once('open', () => console.log(`Connected to database: ${mongoUri}:${mongoPort}`));
+
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
